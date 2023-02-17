745a0e06e3208244ce35094e7cd7b09d672e6c96
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-17 08:51:52 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -Wall $CFILE
+#include <stdio.h>
+
+/**
+ * main -entry pint
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
