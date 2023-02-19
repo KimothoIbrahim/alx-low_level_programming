9e39d4fd3fb0af4994eb282407881e1501e44139
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-19 19:30:18 +0000

    hhhh

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,11 @@ int main(void)
 		for (number = 48; number < 58; number++)
 		{
 		putchar(number);
+		if (number != 57)
+		{
 		putchar(44);
 		putchar(32);
+		}
 		}
 		putchar('\n');
 		return (0);
