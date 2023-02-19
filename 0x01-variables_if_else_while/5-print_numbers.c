268cc6f5bfd1769a12886bb76c0531f215df8284
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-19 17:25:33 +0000

    Print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  *  * main -entry pint
+ *   *   *
+ *    *    * Description: 'the program's description'
+ *     *     *
+ *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{		int number;
+		for (number = 0; number < 10; number++)
+		{
+		printf("%d", number);
+		}
+		printf("\n");
+		return (0);
+}
