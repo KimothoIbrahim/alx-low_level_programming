60df1d79778ec8bc8ac05be389feef4a7a9296c2
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-19 09:25:26 +0000

    Get the last digit from an int variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  *  * main -entry pint
+ *   *   *
+ *    *    * Description: 'the program's description'
+ *     *     *
+ *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, lastNumber;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	lastNumber = n % 10;
+	if (lastNumber > 5)
+	{
+	printf("The last digit of %d is %d and is greater than 5\n", n, lastNumber);
+	}
+	else
+	{
+	if (lastNumber == 0)
+	{
+	printf("The last digit of %d is %d and is 0\n", n, lastNumber);
+	}
+	else
+	{
+	printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lastNumber);
+	}
+	}
+	return (0);
+}
