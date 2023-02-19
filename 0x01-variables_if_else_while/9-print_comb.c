0904487569834d46c8c1e6883ebe11d07e262b8f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-19 19:25:40 +0000

    hhhh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  *  * main -entry pint
+ *   *   *
+ *    *    * Description: 'the program's description'
+ *     *     *
+ *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{		int number;
+
+		for (number = 48; number < 58; number++)
+		{
+		putchar(number);
+		putchar(44);
+		putchar(32);
+		}
+		putchar('\n');
+		return (0);
+}
