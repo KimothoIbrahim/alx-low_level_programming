c438f31e583f4944400f268cd30f3c0eab516a7a
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-19 15:24:47 +0000

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  *  * main -entry pint
+ *   *   *
+ *    *    * Description: 'the program's description'
+ *     *     *
+ *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{		char character = 'a';
+		while (character <= 'z')
+		{
+		printf("%c", character);
+		character++;
+		}
+		return (0);
+}
