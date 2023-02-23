20c964ceddd143a59778a01cddc892f6bc2a01cc
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-23 06:27:25 +0000

    betty mending

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * Main : entry point
- * Description - calls the print_alphabet function
+ * main - entry point
+ * Description : calls the print_alphabet function
  * Return: 0
  */
+
 int main(void)
 {
 	print_alphabet();
