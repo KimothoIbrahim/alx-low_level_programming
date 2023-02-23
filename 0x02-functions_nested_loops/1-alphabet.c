dfcaa10a38aad31a2aaf0800bd555e91128a13ed
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-23 06:16:09 +0000

    print alphabet 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-/*
- * print_alphabet : called by main
- * description : loops from a to z and prints a character to standard out put on every iteration
- * Returns nothing
+/**
+ * print_alphabet - called by main
+ * Description : loops from a to z and prints a
+ * character to standard out put on every iteration
  */
 
 void print_alphabet(void)
-{	
+{
 	/*local variable declaration */
 	char a = 'a';
 
