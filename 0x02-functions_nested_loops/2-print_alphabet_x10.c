d60e8f936d510dd90e4ec9d0ef0a434f54f0d173
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-23 06:55:15 +0000

    for while

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - called by main
+ * Description : loops from a to z and prints a
+ * character to standard out put on every iteration
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	for (n = 0;  n < 10; n++)
+	{
+	/*local variable declaration */
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
