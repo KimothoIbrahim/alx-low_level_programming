22c2f33e9876cdfa4f38a12258dcc3ae0dd079a5
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-24 08:40:46 +0000

    something

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,20 +4,16 @@
 /**
  * print_last_digit - returns the modulo of the number
  * @x : input
- * @y : other input
  *
  * Return: modulo of x
  */
 
 int print_last_digit(int x)
-{	
-	int y;
-
+{
 	if (x < 0)
 	{
-	y = (x % 10) * -1;
-	_putchar('0' + y);
-	return (y);
+	_putchar('0' + ((x % 10) * -1));
+	return (x % 10);
 	}
 	_putchar('0' + (x % 10));
 	return (x % 10);
