b6593c17b8e0afe2ed2418599784a1ba054555b1
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-24 11:46:37 +0000

    hello

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -  adds @a to @b returrns result
+ *
+ * Description - some description
+ *
+ * @a: kkk
+ * @b: jjuuj
+ *
+ *Return: some description
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
