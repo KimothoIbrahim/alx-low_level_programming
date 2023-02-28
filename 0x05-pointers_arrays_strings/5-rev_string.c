4b0ec9f5837ceaf23460eb219d802bf2a2a1861c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-02-28 07:42:49 +0000

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,8 +13,9 @@ void rev_string(char *s)
 	int i;
 	int b = a - 1;
 
-	for (i = 0; i < a; i++)
-	{
+	for (i = 0; i < a / 2; i++)
+	{	char t = *(s + i);
 		*(s + i) = *(s + (b - i));
+		*((s + (b - i))) = t;
 	}
 }
