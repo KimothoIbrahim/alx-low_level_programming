ebb02e8a0bc7429ea9dfc7a3fbc7282677de600c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-03 10:27:00 +0000

    Add toUpper function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - 
+ * @a:
+ *
+ * Return: the pointer *a
+ */
+
+char *string_toupper(char *a)
+{
+	int j;
+
+	for (j = 0; *(a + j) != '\0'; j++)
+	{
+		if ((*(a + j) > 96 && *(a + j) < 123))
+			*(a + j) = (*(a + j) - 32);
+	}
+
+	return (a);
+}
