62b0f22286c0a3032b5668116e6465feecf84332
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-04 14:41:58 +0000

    Add task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@
  */
 
 int _strcmp(char *s1, char *s2)
-{	int i, j;
+{	int i;
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		return (*(s1 + i) - *(s2 + i));
 	}
-	return (j = 0);
+	return (s1[i] - s2[i]);
 }
