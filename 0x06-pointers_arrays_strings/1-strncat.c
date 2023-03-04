cd8512f34e6f58c9c58d3f6f2f41d676b1dc3743
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-04 03:14:54 +0000

    Add smashing prog

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j, length;
 
-	for (i = 0; *(dest + i) != '\n'; i++)
-		length = i - 1;
+	for (i = 0; *(dest + i) != '\0'; i++)
+	{
+		length = i;
+	}
 
 	for (j = 0; j < n; j++)
-		*(dest + (length + j)) = *(src + j);
+	{
+		*(dest + ((length + 1) + j)) = *(src + j);
+	}
 
 	return (dest);
 }
