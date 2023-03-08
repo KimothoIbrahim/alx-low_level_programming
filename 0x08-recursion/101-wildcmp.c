6d419b0c2807053616d2fd82cc864531d05ee57c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-08 18:16:36 +0000

    Add last Task

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * wildcmp - funct
+ * @str1: desc
+ * @str2: desc
+ *
+ * Return: 1 , increment, 0
+ */
+
+int wildcmp(char *str1, char *str2)
+{
+	if (*str1 == '\0' && *str2 == '\0')
+	{
+		/* base case: both strings have reached the end*/
+		return (1);
+	}
+
+		/* strings are identical*/
+	else if (*str1 == *str2)
+	{
+		/* recursive case: characters match, check next characters in each string*/
+		return (wildcmp(++str1, ++str2));
+	}
+	else
+	{
+		/* recursive case: characters don't match, strings are not identical*/
+		return (0);
+	}
+}
+
