c48b7508858b417abfbe44c760f225b6238aed6e
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-11 17:42:33 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print sum of only positive numbers
+ * @argc: desc
+ * @argv: desc
+ *
+ * Return: always returns 0 upon success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error \n");
+				return (1);
+			}
+		}
+
+		printf("%d \n", sum);
+	}
+	else if (argc == 1)
+		printf("0 \n");
+
+	return (0);
+}
