9e20d75cb74463bf30e09d993263a877fc67242e
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-11 13:55:52 +0000

    Add argc task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - entry to programe
+ * @argc: string count parameter
+ * @argv: array of strings parameter
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
 {
 	printf("%d \n", argc - 1);
+	return (0);
 }
