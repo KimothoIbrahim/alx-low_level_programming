d1c8431e24df40992a2c071f20d1684df34c197e
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-12 19:56:29 +0000

    then push

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,6 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0");
-		return (0);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
