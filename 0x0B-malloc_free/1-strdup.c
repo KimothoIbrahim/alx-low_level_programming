a07d18390156df0e7c815f557c58935a833345a9
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-14 06:57:12 +0000

    task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *
+ *
+ * 
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *a;
+
+	if(!str)
+		return (NULL);
+
+	a = malloc(sizeof(str));
+
+	if (a == NULL)
+	{
+		printf("Error : Malloc malfunctioned \n");
+		return (NULL);
+	}
+	
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		a[i] = *(str + i);
+	}
+
+	return (a);
+}
