256a55b1ba3a06c0c9ba26801a0a08c34c18e8ed
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-16 10:46:28 +0000

    Adjust 1-strdup. c file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,20 +3,24 @@
 #include "main.h"
 
 /**
- *
- *
+ * _strdup - copy a string to memory
+ * @str: string to be copied
  * 
+ * Return: returns a pointer to created memory location
  */
 
 char *_strdup(char *str)
 {
-	int i;
+	int i, strlen;
 	char *a;
+
+	for (strlen = 0; *(str + strlen) != '\0';  strlen++)
+		strlen = strlen + 1;
 
 	if(str == NULL)
 		return (NULL);
 
-	a = malloc(sizeof(str) + 1);
+	a = malloc(sizeof(char) * strlen);
 
 	if (a == NULL)
 	{
