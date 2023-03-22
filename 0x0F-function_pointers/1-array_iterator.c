b55968ff1e128fad758b3a6db92f3f9507f10e56
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-22 14:29:20 +0000

    Adjust task two: Function Pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {	unsigned long i;
 
-	for (i = 0; i < size; i++)
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
 		action(array[i]);
+	}
 }
