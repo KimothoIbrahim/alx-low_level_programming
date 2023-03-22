ea99100c5dcc42e5ac37e9f6f2e5c758e440ecc4
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-03-22 11:37:22 +0000

    Add task 1: Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,12 @@
 
 /**
  * print_name - function calls a function
- * pointer and passes it a name which it then prints
+ * 		pointer and passes it a name which it then prints
  * @name: pointer to a name
  * @f: pointer to the passed funtion
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(*f)(name);
 }
