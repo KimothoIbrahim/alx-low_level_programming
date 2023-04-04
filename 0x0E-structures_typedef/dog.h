63b66fe0990818a7ca835bac5824364866de4e44
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-04-04 04:12:27 +0000

    Correct to protect from double inclusion

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - description
  * @name: name
@@ -17,3 +20,5 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+#endif
