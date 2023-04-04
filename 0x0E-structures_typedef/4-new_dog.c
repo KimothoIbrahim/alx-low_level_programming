320c0c6261381cbd87b9e4747f9ef9af9f266d03
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-04-04 15:13:20 +0000

    Update task 5: structures and Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,51 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-
-/**
- * new_dog - creates a new dog
- *
- * @name: argument with name of dog
- * @age: argument with age of dog
- * @owner: argument with dog owner's name
- *
- * Return: structure of type dog
- */
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *dog;
-	int len1, len2;
-
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-
-	if (dog->owner == NULL)
-	{
-		free(dog);
-		free(dog->name);
-		return (NULL);
-	}
-
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
-
-	return (dog);
-}
 
 /**
  * _strlen - returns string length
@@ -94,4 +48,49 @@ char *_strcpy(char *dest, char *src)
 	dest[k] = '\0';
 
 	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: argument with name of dog
+ * @age: argument with age of dog
+ * @owner: argument with dog owner's name
+ *
+ * Return: structure of type dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
