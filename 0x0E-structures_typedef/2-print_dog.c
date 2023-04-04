e3cf42bee50ab635de34e835253af4d40143001d
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-04-04 08:30:35 +0000

    Correct structure print function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@
  * @d: pointer to dog structure
  */
 
-void print_dog(struct dog *d)
+/*void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
@@ -30,4 +30,17 @@ void print_dog(struct dog *d)
 	}
 	else
 		printf("Owner: %s\n", d->owner);
+}*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
