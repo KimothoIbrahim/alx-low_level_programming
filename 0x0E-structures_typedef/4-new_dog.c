65f2c719763689c9a7076ff4dcccd56340eea77d
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-04-04 14:38:12 +0000

    Add task 5: Structures and Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,3 +47,51 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (dog);
 }
 
+/**
+ * _strlen - returns string length
+ * @s: string to check
+ *
+ * Return: string length
+ * */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: pointer to created memory space 
+ * @src: string copied from
+ *
+ * Return: pointer to string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, k;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (k = 0; k < len; k++)
+	{
+		dest[k] = src[k];
+	}
+	dest[k] = '\0';
+
+	return (dest);
+}
