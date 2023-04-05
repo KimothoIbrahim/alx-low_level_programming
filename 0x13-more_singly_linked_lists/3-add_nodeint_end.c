3a1ac38f0e114f905481b84ef9d387e6c1ca8017
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-04-05 21:02:06 +0000

    Add task 4: more linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * add_nodeint_end - node at end of list
@@ -11,13 +12,12 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *endNode = *head;
 
-	listint_t *newNode = (listint_t) malloc(sizeof(listint_t));
+	listint_t *newNode = (listint_t *) malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 		return (NULL);
-
-	listint_t *endNode = *head;
 
 	newNode->n = n;
 
