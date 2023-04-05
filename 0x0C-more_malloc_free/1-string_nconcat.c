9043632010898f541526652435028649f0cbd6ef
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-04-05 19:56:29 +0000

    Add task 2: more malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>		
+#include <stdlib.h>
 #include "main.h"
-		
+
 /**
  * string_nconcat - concatenates
  * @s1: desc
@@ -9,12 +9,12 @@
  *
  * Return: pointer to the resulting string
  */
-		
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-	
+
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
