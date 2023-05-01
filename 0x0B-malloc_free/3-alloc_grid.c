921b7d6cd54380657e6acef672fd64e8aa41213b
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-01 17:52:01 +0000

    Edit malloc free task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,52 +1,42 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * alloc_grid - make a 2D array
- * @width: desc
- * @height: height
+ * alloc_grid - allocates memory for a @D array
+ * @width: number of integer per row
+ * @height: rows of the 2D arry
  *
- * Return: pointer to array
+ * Return: pointer to 2d array
  */
 
 int **alloc_grid(int width, int height)
 {
-	int **b;
-	int i, j, k;
+	int **pointer;
+	int i, j;
 
-	if (width == 0 || height == 0)
+	if (width < 0 || height < 0)
 		return (NULL);
 
-	b = malloc(sizeof(int *) * height);
-
-	if (b == NULL)
-	{
-		free(b);
+	pointer = malloc(sizeof(int *) * height);
+	if (pointer == NULL)
 		return (NULL);
-	}
-	else
+
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
+		pointer[i] = malloc(sizeof(int) * width);
+		if (pointer[i] == NULL)
 		{
-			b[i] = malloc(sizeof(int) * width);
-			
-			if (b[i] == NULL)
-			{
-				free(b);
-				return (NULL);
-			}
+			for ( ; i >= 0; i--)
+				free(pointer[i]);
+
+			free(pointer);
+			return (NULL);
 		}
-		
-		for (j = 0; j < height; j++)
-		{
-			for (k = 0; k < width; k++)
-			{
-				b[j][k] = 0;
-			}
-		}
-		
-		return (b);
 	}
 
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			pointer[i][j] = 0;
+
+	return (pointer);
 }
