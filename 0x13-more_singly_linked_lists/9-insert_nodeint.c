7fb1fbbcf0a3ceaa54015055da1ad80a33bfeda4
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-02 14:25:04 +0000

    Edit task more singly linked lists

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inser new node at given index
+ * @head: pointer to start of list
+ * @idx: index to insert new node
+ * @n: value of new node
+ *
+ * Return: pointer to the new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode, *movingPointer;
+	unsigned int index = 0;
+
+	movingPointer = *head;
+
+	while (movingPointer)
+	{
+		if ((idx - 1) == index)
+		{
+			newNode = malloc(sizeof(listint_t));
+			if (!newNode)
+				return (NULL);
+			newNode->n = n;
+			newNode->next = movingPointer->next;
+			movingPointer->next = newNode;
+			return (newNode);
+		}
+
+		index++;
+		movingPointer = movingPointer->next;
+	}
+	return (NULL);
+}
