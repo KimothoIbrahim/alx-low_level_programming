66027be615fd9ef1bcf6c670ca7273528af99bc0
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-02 14:35:40 +0000

    Edit task more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	movingPointer = *head;
 
+	if (idx == 0)
+	{
+		newNode = malloc(sizeof(listint_t));
+		if (newNode == NULL)
+			return (NULL);
+		neNode->n = n;
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
 	while (movingPointer)
 	{
 		if ((idx - 1) == index)
