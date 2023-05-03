459e6e78be2add4e043da242c6966b506d00759f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-03 08:39:15 +0000

    Edit 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,29 +2,38 @@
 #include <string.h>
 
 /**
- * _atoi - desc
- * @s: desc
+ * _atoi - converts a string to an integer
+ * @s: given string
  *
  * Return : 0 if no number present, number if number present
  */
 
 int _atoi(char *s)
 {
-	int a = strlen(s);
-	int i, n, number;
-	int x;
+	int i = 0, a = 0, n = 0, length = 0, b = 0, number = 0;
 	
-	x= 1;
-	number = 0;
-
-	for (i = a - 1; i >= 0; i--)
+	while (s[length] != '\0')
+		length++;
+	
+	while (i < length && b == 0)
 	{
-		if (s[i] >= 48 && s[i] <= 57)
+		if (s[i] == '-')
+			++a;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n = s[i];
-			number += ((n - 48) * x);
-			x *= 10;
+			number = s[i] - '0';
+			if (a % 2)
+				number = -number;
+			n = n * 10 + number;
+			b = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			b = 0;
 		}
+		i++;
 	}
-	return (number);
+
+	if (b == 0)
+		return (0);
+	return (n);
 }
