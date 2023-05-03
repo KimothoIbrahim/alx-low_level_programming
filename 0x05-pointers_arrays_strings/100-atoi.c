08ebe8e6c405bbc6a769316b1942b510311b60ad
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-03 08:41:49 +0000

    Edit 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,16 +5,16 @@
  * _atoi - converts a string to an integer
  * @s: given string
  *
- * Return : 0 if no number present, number if number present
+ * Return: 0 if no number present, number if number present
  */
 
 int _atoi(char *s)
 {
 	int i = 0, a = 0, n = 0, length = 0, b = 0, number = 0;
-	
+
 	while (s[length] != '\0')
 		length++;
-	
+
 	while (i < length && b == 0)
 	{
 		if (s[i] == '-')
