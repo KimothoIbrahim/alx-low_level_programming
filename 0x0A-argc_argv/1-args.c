2a7b1f561e110c3d405d0c09f183adfb6daa041c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-03 17:10:38 +0000

    Edit 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry to programe
@@ -8,8 +9,9 @@
  * Return: always returns 0
  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d \n", argc - 1);
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
