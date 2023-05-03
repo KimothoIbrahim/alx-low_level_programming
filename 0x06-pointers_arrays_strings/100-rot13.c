22b37f1f74403afc43a5adff4d3062e277dbfff9
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-03 09:55:35 +0000

    Edit 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string
+ *
+ * Return: pointer to rotated string
+ */
+
+char *rot13(char *a)
+{
+	int i, j;
+	char string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotated[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[i] == string[j])
+			{
+				a[i] = rotated[j];
+				break;
+			}
+		}
+	}
+	return (a);
+}
