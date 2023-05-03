934c3631d2233ed56569bfdc9a72a6a6ced901e2
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-03 17:19:00 +0000

    Adjust 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - function
- * @argc: desc
- * @argv: desc
+ * main - prints all recieved arguments
+ * @argc: number of input arguments
+ * @argv: array of input arguments
  *
- * Return: always returns 0
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
-{	int i;
+{
+	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s \n", *(argv + i));
+		printf("%s\n", argv[i]);
 	}
-
 	return (0);
 }
+
