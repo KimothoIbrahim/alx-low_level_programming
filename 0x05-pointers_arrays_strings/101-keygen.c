31060c4a747d8edc5ec276634c6249a07bf5e39b
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-03 08:54:19 +0000

    Add task 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random password generator
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int password[100];
+	int i, sum = 0, a;
+
+	srand(time(NULL));
+	
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			a = 2772 - sum - '0';
+			sum += a;
+			putchar(a + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
