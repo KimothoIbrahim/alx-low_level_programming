87492ac59bd3a2456657107353eca701639ce749
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-05-09 02:40:27 +0000

    Add task 1: file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[len];)
+		for (length = 0; text_content[length];)
 			length++;
 	}
 
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	written = write(fd, text_content, length);
+	written = write(file_descriptor, text_content, length);
 
 	if (file_descriptor == -1 || written == -1)
 		return (-1);
