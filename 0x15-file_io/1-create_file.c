8fba7afbc50ed06260562b47319c866f4de7dfaa
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-07-10 14:38:08 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fdesc, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 
@@ -20,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fdesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fdesc, text_content, len);if (fdesc == -1 || w == -1)
+	w = write(fdesc, text_content, len);
+	if (fdesc == -1 || w == -1)
 		return (-1);
 
 	close(fdesc);
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - attach text to end of a file.
+ * @filename: pointer to file
+ * @text_content:
+ *
+ * Return: -1 failure Otherwise 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int p, d, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	p = open(filename, O_WRONLY | O_APPEND);
+	d = write(o, text_content, len);
+
+	if (p == -1 || d == -1)
+		return (-1);
+	close(p);
+	return (1);
+}
