d2443df2f52fc1223109393414e778401a90f3ad
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-07-10 19:31:30 -0700

    Correct task 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	p = open(filename, O_WRONLY | O_APPEND);
-	d = write(o, text_content, len);
+	d = write(p, text_content, len);
 
 	if (p == -1 || d == -1)
 		return (-1);
